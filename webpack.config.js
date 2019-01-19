914a073dfcad62c41a9699759c58fb3993db69c5
Onkar Joshi <onkarj422@gmail.com>
2019-01-20 03:49:11 +0530

    [base/webpack-config] basic react eslint  babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,58 @@
 /* webpack.config.js */
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: path.join(__dirname, 'src', 'index.html'),
-    filename: "./index.html"
+    template: path.join(__dirname, 'app', 'index.html'),
+    filename: './index.html',
 });
 
 module.exports = {
     /* ... */
     // Tell webpack to begin building its
     // dependency graph from this file.
-    entry: path.join(__dirname, 'src', 'index.js'),
+    entry: path.join(__dirname, 'app', 'index.js'),
     // And to place the output in the `build` directory
     output: {
         path: path.join(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
+                test: /\.(jsx|js)$/,
                 /* We'll leave npm packages as is and not
                    parse them with Babel since most of them
                    are already pre-transpiled anyway. */
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: 'babel-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader'],
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.(jpe?g|png|gif)$/,
-                use: [{
-                    /* inline if smaller than 10 KB, otherwise load as a file */
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000
-                    }
-                }]
+                use: [
+                    {
+                        /* inline if smaller than 10 KB, otherwise load as a file */
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(eot|svg|ttf|woff2?|otf)$/,
-                use: 'file-loader'
-            }
-        ]
+                use: 'file-loader',
+            },
+        ],
     },
-    plugins: [htmlPlugin]
-}
+    plugins: [htmlPlugin],
+};
