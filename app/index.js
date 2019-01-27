e06702c1ab49ddc8d19dd2821667061657372be0
Onkar Joshi <onkarj422@gmail.com>
2019-01-28 00:51:58 +0530

    [base] added material ui library

--- app/index.js
+++ app/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
+import theme from './theme/material-ui/theme';
 import Router from './components/Router';
 import routes from './router/routeConfig';
 import App from './App';
 
+const rootStyles = {
+    '@global': {
+        body: {
+            ...theme.typography.body1
+        },
+    },
+}
+
+const AppWithRootStyles = withStyles(rootStyles)(App)
+
 const render = () => (
     <BrowserRouter>
-        <App title="React with Router">
-            <Router routes={routes} />
-        </App>
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline>
+                <AppWithRootStyles>
+                    <Router routes={routes} />
+                </AppWithRootStyles>
+            </CssBaseline>
+        </MuiThemeProvider>
     </BrowserRouter>
 );
 
---
+++ app/theme/material-ui/theme.js
@@ -0,0 +1,24 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import pink from '@material-ui/core/colors/pink';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: lightBlue,
+        secondary: pink,
+        error: red,
+        // Used by `getContrastText()` to maximize the contrast between the background and
+        // the text.
+        contrastThreshold: 3,
+        // Used to shift a color's luminance by approximately
+        // two indexes within its tonal palette.
+        // E.g., shift from Red 500 to Red 300 or Red 700.
+        tonalOffset: 0.2,
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+export default theme;
